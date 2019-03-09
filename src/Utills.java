b13952ec3968e07762cfaf9cd8db99f0f6ccd06d
Daniel <daniel@opus1.io>
2019-03-09 15:22:46 -0800

    Add parse2016ElectionResults() method into Utills

--- src/Utills.java
+++ src/Utills.java
@@ -19,4 +19,65 @@ public class Utills {
 
         return output.toString();
     }
+
+    public void parse2016ElectionResults(String data){
+        ArrayList<ElectionResult> list = new ArrayList<ElectionResult>();
+        String[] a = data.split("/n");
+        for(String s : a){
+            ElectionResult currResult = new ElectionResult();
+            for (int i = 0; i < s.length(); i++){
+                char c = s.charAt(i);
+                if(c == '"'){
+                    String tempString = "";
+                    for(int j = i + 1; s.charAt(j) != '"'; i++){
+                        char c2 = s.charAt(j);
+                        if(c2 != ','){
+                            tempString += c2;
+                        }
+                    }
+                    currResult.setPer_gop(Double.parseDouble(tempString));
+                }
+                String[] categorys = s.split(",");
+
+                currResult.setVotes_dem(Double.parseDouble(categorys[1]));
+
+                currResult.setVotes_gop(Double.parseDouble(categorys[2]));
+
+                currResult.setTotal_votes(Double.parseDouble(categorys[3]));
+
+                currResult.setPer_dem(Double.parseDouble(categorys[4]));
+
+                String tempStrings = "";
+                for(int j = 0; categorys[6].charAt(j) != '%'; i++){
+                    char c3 = categorys[6].charAt(j);
+                    if(c3 != '%'){
+                        tempStrings += c3;
+                    }
+                }
+                currResult.setPer_point_dif(Double.parseDouble(tempStrings));
+
+                currResult.setState_abbr(categorys[7]);
+
+                currResult.setCounty_name(categorys[8]);
+
+                currResult.setCombined_flips(Integer.parseInt(categorys[9]));
+
+
+
+            }
+        }
+    }
+
+    public String removeCommas(String string){
+        String temp = "36,296";
+        int indexOfComma = -1;
+        for (int i = 0; i < temp.length(); i++) {
+            char c = temp.charAt(i);
+            if (c == ',') indexOfComma = i;
+        }
+
+        String result = temp.substring(0, indexOfComma) + temp.substring(indexOfComma + 1);
+
+        return result;
+    }
 }
