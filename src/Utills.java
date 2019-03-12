5b91757191dbff4997c934465f634cd47c9b09b8
dburstein163 <danielburstein123@gmail.com>
2019-03-12 11:18:59 -0700

    fix errors and clean code skills

--- src/Utills.java
+++ src/Utills.java
@@ -25,18 +25,10 @@ public class Utills {
         String[] a = data.split("/n");
         for(String s : a){
             ElectionResult currResult = new ElectionResult();
-            for (int i = 0; i < s.length(); i++){
-                char c = s.charAt(i);
-                if(c == '"'){
-                    String tempString = "";
-                    for(int j = i + 1; s.charAt(j) != '"'; i++){
-                        char c2 = s.charAt(j);
-                        if(c2 != ','){
-                            tempString += c2;
-                        }
-                    }
-                    currResult.setPer_gop(Double.parseDouble(tempString));
-                }
+            String s2 = StringCleaner(s);
+
+
+
                 String[] categorys = s.split(",");
 
                 currResult.setVotes_dem(Double.parseDouble(categorys[1]));
@@ -46,7 +38,7 @@ public class Utills {
                 currResult.setTotal_votes(Double.parseDouble(categorys[3]));
 
                 currResult.setPer_dem(Double.parseDouble(categorys[4]));
-
+                currResult.setPer_gop(Double.parseDouble(categorys[5]));
                 String tempStrings = "";
                 for(int j = 0; categorys[6].charAt(j) != '%'; i++){
                     char c3 = categorys[6].charAt(j);
@@ -64,7 +56,7 @@ public class Utills {
 
 
 
-            }
+
             list.add(currResult);
 
         }
@@ -83,4 +75,26 @@ public class Utills {
 
         return result;
     }
+}
+
+
+
+public static String StringCleaner(String s){
+    String tempString="";
+        for(int i=0;i<s.length();i++){
+        char c=s.charAt(i);
+        if(c=='"'){
+        for(int j=i+1;s.charAt(j)!='"';i++){
+        char c2=s.charAt(j);
+        if(c2!=','){
+        tempString+=c2;
+            }
+                 }
+                }
+                if(c!= '%'){
+        tempString+=c;
+        }
+            }
+            return tempString;
+        }
 }
